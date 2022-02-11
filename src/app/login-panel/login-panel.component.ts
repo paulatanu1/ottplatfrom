e2be475a1df1b4f15a7a807d69bc4b8effc8ec04
ATANU PAUL <atanupaul76@gmail.com>
2022-02-11 22:45:43 +0530

    login-panel function done

--- src/app/login-panel/login-panel.component.ts
+++ src/app/login-panel/login-panel.component.ts
@@ -1,27 +1,44 @@
-import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { userdetails } from '../appServices/userdetails.service';
+import { Router } from '@angular/router';
+import { CountdownConfig } from 'ngx-countdown';
+
+
 
 @Component({
   selector: 'app-login-panel',
   templateUrl: './login-panel.component.html',
-  styleUrls: ['./login-panel.component.scss']
+  styleUrls: ['./login-panel.component.scss'],
+  providers: [userdetails]
 })
 export class LoginPanelComponent implements OnInit {
   usernumber: string = '';
-
-  @Output() gettingNo = new EventEmitter();
-  mobnumber:string ='';
-  constructor() { }
-  clickme(e:any) {
-    if(e.value.length>0){
-      // alert(e.value)
-      this.gettingNo.emit(e.value)
-    }
-    
+  otpmsg:boolean = false;
+  userno:any=''
+  config: CountdownConfig = {
+    leftTime: 5,
+    formatDate: ({ date }) => `${date / 1000}`,
   };
-  getnumber(){
-    console.log(this.mobnumber);
-  }
+
+  // mobnumber:string ='';
+  constructor(private userdetails :userdetails,private router: Router) { }
+  
+  // getnumber(){
+  //   console.log(this.mobnumber);
+  // }
   ngOnInit(): void {
+    
+  }
+
+  onAddmob(){
+    this.userdetails.addNumber(this.usernumber);
+    this.otpmsg=true;
+    // this.usernumber = this.userno
+    // console.log(this.usernumber);
+    setTimeout(()=>{                           // <<<---using ()=> syntax
+      this.router.navigateByUrl('/login-panel/Login');
+  }, 5000);
+
   }
 
 
